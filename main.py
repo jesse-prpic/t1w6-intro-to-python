961f6b050b166305281a68ec2944534865befe48
jesse-prpic <13812@coderacademy.edu.au>
2024-06-06 19:02:02 +1000

    catch up on class

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-print ("Hello World!!")
-print (5 - 3)
-print ("Don't forget to commit!")
+# print ("Hello World!!")
+# print (5 - 3)
+# print ("Don't forget to commit!")
+
+# # print("It's a comment, it won't be printed")
+
+# #Data types
+# #integers
+# # 4
+# # 7
+# # 9
+# # print (9)
+# # print ("9")
+# # print (type(9))
+# # print (type("9"))
+
+# #floats
+# 2.5
+# 5.123123
+# print(3.1415)
+# print(type(-5.63))
+# print(type(-5))
+
+# #complex (imaginary numbers), no worries, we won't cover these kind of numbers in the course
+# 2 + 3
+# print(type(4+7j))
+
+#operators + - * / %
+print (4 * 5 )
+print (15 / 3)
+print (15 /2 )
+print (7 % 2)
